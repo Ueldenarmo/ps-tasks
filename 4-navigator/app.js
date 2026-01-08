4f431a52898a4ea0911807fd3ba5742c283cd0b0
Ueldenarmo <gerygixer@gmail.com>
2026-01-08 17:53:47 +0300

    feat: add check

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -1,5 +1,15 @@
 const calculateDistance = (addressLat, addressLong, positionLat, positionLong) => {
+    if (
+        typeof addressLat !== "number" ||
+        typeof addressLong !== "number" ||
+        typeof positionLat !== "number" ||
+        typeof positionLong !== "number"
+    ) {
+        console.error(`Только циферки, пжлст!`);
+        return false;
+    }
     return Math.sqrt((positionLat - addressLat) ** 2 + (positionLong - addressLong) ** 2);
 };
 
-console.log(calculateDistance(2, 2, 50, 50));
+console.log(calculateDistance(6, 89, 15, 50));
+console.log(calculateDistance(6, "кися", 15, 50));

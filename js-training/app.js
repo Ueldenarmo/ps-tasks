61cb8fd5aba79fcd4dab3607387a812a72fa4864
Ueldenarmo <gerygixer@gmail.com>
2026-01-16 14:01:31 +0300

    save for training

--- js-training/app.js
+++ js-training/app.js
@@ -1,1 +1,5 @@
-console.log("User" && "Moderator");
+function calculateDiscountedPrice(price, discountPercent) {
+    const discountSum = (price, discountPercent) => price * (discountPercent / 100);
+    return price - discountSum(price, discountPercent);
+}
+console.log(calculateDiscountedPrice(75, 90));

38f4be5d190e5f33c332f067364a4e6d754909e1
Ueldenarmo <gerygixer@gmail.com>
2026-01-23 17:06:02 +0300

    some changes for training

--- js-training/app.js
+++ js-training/app.js
@@ -1,3 +1,14 @@
-const roles = ["developer", "admin", "user", "superuser", "manager"];
-roles.push("test");
-console.log(roles);
+const numbers = [3, 7, 2, 9, 1];
+const threshold = 10;
+
+let found = false;
+for (let i = 0; i < numbers.length; i++) {
+    found = true;
+    if (numbers[i] > threshold) {
+        console.log(numbers[i]);
+        break;
+    }
+}
+if (!found) {
+    console.log("Элемент не найден");
+}

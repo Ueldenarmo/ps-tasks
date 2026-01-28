5fcc6ab8b18f5cd8dd1402f018aa27d481c72fb6
Ueldenarmo <gerygixer@gmail.com>
2026-01-28 18:13:32 +0300

    save for training

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -2,7 +2,6 @@ let arr = [1, 40, -5, 10, 0];
 
 for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
-        // ← ИСПРАВЬ ЗДЕСЬ
         if (arr[j] > arr[j + 1]) {
             let temp = arr[j];
             arr[j] = arr[j + 1];

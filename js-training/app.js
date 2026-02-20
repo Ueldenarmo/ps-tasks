67b20220bf0ab662334f1757b14aebfc81459cd1
Ueldenarmo <gerygixer@gmail.com>
2026-02-20 13:00:34 +0300

    save for training

--- js-training/app.js
+++ js-training/app.js
@@ -1,25 +1,5 @@
-// function createDivider(divisor) {
-//     if (divisor !== 0) {
-//         return function doDivide(value) {
-//             return value / divisor;
-//         };
-//     }
-//     console.error(`Делитель не должен быть равен нулю`);
-// }
+const numbers = [15, 28, 42, 7, 33];
+const target = 25;
 
-// Тестирование
-// const divideByTwo = createDivider(0);
-// console.log(divideByTwo(10)); // 5
-
-// const divideByFour = createDivider(4);
-// console.log(divideByFour(20)); // 5
-
-function calculate(x, y, operation) {
-    return operation(x, y);
-}
-
-function createPower(power) {
-    return function doPower(value) {
-        return value ** power;
-    };
-}
+let isTargetNumber = numbers.some((num) => num === target);
+console.log(isTargetNumber);

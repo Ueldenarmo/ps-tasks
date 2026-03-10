048586bde72d23068c8864a2b7365b51f34994f6
Ueldenarmo <gerygixer@gmail.com>
2026-03-10 15:55:15 +0300

    add saving for js-training

--- js-training/app.js
+++ js-training/app.js
@@ -1,11 +1,2 @@
-"use strict";
-
-const number = 42;
-const person = { name: "Alice" };
-
-console.log(`Значение числа: ${number}`);
-console.log(`Имя до изменения: ${person.name}`);
-
-person.name = "Bob";
-
-console.log(`Имя после изменения: ${person.name}`);
+let f = func.bind(obj, 1);
+f(2);

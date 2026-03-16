a34e57219ea856fef189a8372d5d8840380415e0
Ueldenarmo <gerygixer@gmail.com>
2026-03-16 17:29:42 +0300

    save for DOM part

---
+++ DOM 17-2/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function submitForm() {
+    const input = document.querySelector(".input").value;
+    console.log(input);
+    if (!input) {
+        return;
+    }
+    document.querySelector(".panel").innerText = input;
+    document.querySelector(".input").value = "";
+    document.querySelector(".notification").classList.add("notification_active");
+    const textString = JSON.stringify({
+        text: input,
+    });
+    localStorage.setItem("text", textString);
+}
+
+function inputChanged(e) {
+    if (e.code == "Enter") {
+        submitForm();
+    }
+}
+
+console.log(document);
